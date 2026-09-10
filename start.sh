b78c50d339071588ddddf91fff0e9be4839ccb3a
unknown <christophe1213.com@gmail.com>
2026-09-10 13:17:21 +0300

    Mise à jour

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ set -e
 
 git init -q
 
+echo "DAGSHUB_USER défini : ${DAGSHUB_USER:+oui}"
+echo "DAGSHUB_TOKEN défini : ${DAGSHUB_TOKEN:+oui}"
 dvc remote modify origin --local auth basic
 dvc remote modify origin --local user "$DAGSHUB_USER"
 dvc remote modify origin --local password "$DAGSHUB_TOKEN"
